fd193c15319065eeb171a1e0c7a149d62c94633b
ShraddhaManwar <shraddha.manwar@neosofttech.com>
2022-01-18 14:02:28 +0530

    changes for view

--- SampleTask.IOS/Cells/CollectionViewCellType_1.designer.cs
+++ SampleTask.IOS/Cells/CollectionViewCellType_1.designer.cs
@@ -1,18 +1,42 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace SampleTask.IOS.Cells
 {
-    [Register("CollectionViewCellType_1")]
-    partial class CollectionViewCellType_1
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("CollectionViewCellType_1")]
+	partial class CollectionViewCellType_1
+	{
+		[Outlet]
+		UIKit.UILabel lblCustAge { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblCustName { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblTestmon { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (lblTestmon != null) {
+				lblTestmon.Dispose ();
+				lblTestmon = null;
+			}
+
+			if (lblCustName != null) {
+				lblCustName.Dispose ();
+				lblCustName = null;
+			}
+
+			if (lblCustAge != null) {
+				lblCustAge.Dispose ();
+				lblCustAge = null;
+			}
+		}
+	}
 }
---
+++ SampleTask.IOS/Dto/CustomerDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SampleTask.IOS.Dto
+{
+    public class CustomerDto
+    {
+        public CustomerDto()
+        {
+        }
+        public string CustomerImage { get; set; }
+        public string TestImage { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAge { get; set; }
+        public string CustomerTest { get; set; }
+        public string Days { get; set; }
+        public string TestName { get; set; }
+    }
+}
